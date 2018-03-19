c47591a2c346e0af17abf7641e270d100a942fd9
ajvignesh@gmail.com <indian1404$>
2018-03-19 22:11:55 +0300

    added error dialog

--- src/main/resources/static/js/signupController.js
+++ src/main/resources/static/js/signupController.js
@@ -19,33 +19,55 @@ mySpringApp.controller('signupController', function($rootScope, $scope, $http, $
 	    phoneNum : $scope.phoneNum
 	}).then(
 		function(response) {
-		    
+
 		    var data = response.data;
 		    if (data.errorsFound != undefined && data.errorsFound === 'Y') {
-			
-			$scope.open();
+
+			$scope.open(data.errorMessagesMap);
 		    } else {
-			
+
 			alert('User account created successfully. Please sign-in.');
 			$location.path("/login");
 		    }
 		}),
 		function(error) {
-	    		$location.path("/login");
-	    		$scope.error = true;
-	    		$rootScope.authenticated = false;
-		};
+        	    $location.path("/login");
+        	    $scope.error = true;
+        	    $rootScope.authenticated = false;
+	};
     }
 
-    $scope.open = function () {
+    $scope.open = function(errorDetails) {
 	var modalInstance = $uibModal.open({
-	    templateUrl: '/templates/errors.html',
-	    controller: 'errorController',
+	    templateUrl : '/templates/errors.html',
+	    controller : 'errorController',
+	    resolve : {
+		errorDetailsTxt : function() {
+		    return errorDetails;
+		}
+	    }
 	});
     }
 });
 
-mySpringApp.controller('errorController', function ($scope, $uibModalInstance) {
+mySpringApp.controller('errorController', function ($scope, $uibModalInstance, errorDetailsTxt) {
+
+    /*
+     * Sample data:
+     * 
+     * errorDetailsTxt = {"firstName":"First name cannot be empty","mailId":"Mail id cannot be empty","username":"Username already exist"}
+     */
+    var errorDetailsList = [];
+    for (var key in errorDetailsTxt) {
+	if (errorDetailsTxt.hasOwnProperty(key)) {
+	    // alert(key + " -> " + p[key]);
+
+	    errorDetailsList.push(errorDetailsTxt[key]);
+	}
+    }    
+
+    $scope.errorDetailsList = errorDetailsList;
+
     $scope.close = function () {
 	$uibModalInstance.dismiss('cancel');
     };
